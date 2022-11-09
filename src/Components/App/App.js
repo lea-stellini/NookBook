14df74a90c73c464a7313343b285b3a88f2c6d3a
lea-stellini <lea.stellini@gmail.com>
2022-11-09 16:00:07 +0100

    updated App files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.png';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { FaUserAlt } from "react-icons/fa";
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      searchResults: [
+      {name: 'name1', birthday: 'birthday1', image: 'image1', motto: 'motto1', id: 1 },
+      {name: 'name2', birthday: 'birthday2', image: 'image2', motto: 'motto2', id: 2 },
+      {name: 'name3', birthday: 'birthday3', image: 'image3', motto: 'motto3', id: 3 }]
+    }
+  }
+
+  render() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} alt="Logo" className="logo" />
+      <SearchBar />
+      <FaUserAlt className="user"/>
       </header>
+      <div className="App">
+        <SearchResults searchResults={this.state.searchResults} />
+      </div>
     </div>
   );
+}
 }
 
 export default App;
