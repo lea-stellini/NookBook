5b9d4bdfd3c0368c8c37d05ce20453d9427900df
lea-stellini <lea.stellini@gmail.com>
2022-11-09 16:35:21 +0100

    updated ProfilList.js

---
+++ src/Components/ProfilList/ProfilList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Profil } from '../Profil/Profil';
+
+export class ProfilList extends React.Component {
+
+    render(){
+
+        return (
+            <div className="ProfilList">
+                {
+                        this.props.profils.map(profil => {
+                            return <Profil profil={profil}
+                                                key={profil.id}/>
+                        })
+                    }
+            </div>
+        )
+    }
+}
