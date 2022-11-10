70924853ac0de360eff804358bca37e76b134b72
lea-stellini <lea.stellini@gmail.com>
2022-11-10 15:41:12 +0100

    added a font

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -6,7 +6,7 @@ export class SearchResults extends React.Component {
     render(){
         return (
             <div className="Search">
-                <h1>Were you looking for :</h1>
+                <h1>Results :</h1>
                 <div className="SearchResults">
                     <ProfilList profils={this.props.searchResults}/>
                 </div>
